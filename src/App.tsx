723a64d582a198727f64edfbd3dd2da5ee335fdf
matsuhashi-tatsuya <matsuhashi-tatsuya@dmm.com>
2023-08-24 23:28:45 +0900

    react router導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import type { FC } from 'react';
+import { useEffect, type FC } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import LiveList from './components/LiveList';
 import theme from './theme';
 
 const App: FC = () => {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (hash.length === 0) {
+      window.scrollTo(0, 0);
+    }
+  }, [hash, pathname]);
+
   return (
     <div>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
-        <LiveList />
+        <Routes>
+          <Route path="/" element={<LiveList />} />
+        </Routes>
       </ThemeProvider>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -6,9 +7,11 @@ import Typography from '@mui/material/Typography';
 const Header: FC = () => (
   <AppBar position="sticky">
     <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Live
-      </Typography>
+      <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Live
+        </Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 );
