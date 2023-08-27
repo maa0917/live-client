53b02409b8b96f24911da3bf9f31fb13b10f6ade
matsuhashi-tatsuya <matsuhashi-tatsuya@dmm.com>
2023-08-28 00:48:28 +0900

    uuidに変更

--- src/hooks/useLives.tsx
+++ src/hooks/useLives.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 type Live = {
-  id: number;
+  uuid: string;
   eventDate: string;
   title: string;
   venue: string;
@@ -19,56 +19,56 @@ const useLives = (): UseLivesReturnType => {
     const load = () => {
       const livesData = [
         {
-          id: 1,
+          uuid: '4a7b8df2-a290-44b6-bc98-90a72b5f14c5',
           eventDate: '2023.11.18(Sat)',
           title: 'D×D×J 2023 -TOKYO-',
           venue: '渋谷CLUB QUATTRO',
           acts: ['DEVILOOF', 'DEXCORE', 'JILUKA'],
         },
         {
-          id: 2,
+          uuid: '6ded12f9-1287-42ca-8354-dfc7fcc4a9d4',
           eventDate: '2023-09-03T09:00:00.000Z',
           title: 'FORWARD RELEASE TOUR 2023',
           venue: '新潟club RIVERST',
           acts: ['Azami'],
         },
         {
-          id: 3,
+          uuid: '7b8e853f-dbbb-44ab-9f88-77f871ee2ddb',
           eventDate: '2023-08-26T09:00:00.000Z',
           title: 'FORWARD RELEASE TOUR 2023',
           venue: '岡山CRAZY MAMA 2nd room',
           acts: ['Azami'],
         },
         {
-          id: 4,
+          uuid: '9b41a946-ae91-4678-9c33-9e7a4e41871e',
           eventDate: '2023-09-09T09:00:00.000Z',
           title: 'FORWARD RELEASE TOUR 2023',
           venue: '甲府KAZOO HALL',
           acts: ['Azami'],
         },
         {
-          id: 5,
+          uuid: 'f6c8e643-b19b-4843-a456-1a0f73f8fabc',
           eventDate: '2023-08-13T09:00:00.000Z',
           title: 'FORWARD RELEASE TOUR 2023',
           venue: '仙台enn 3rd',
           acts: ['Azami'],
         },
         {
-          id: 6,
+          uuid: 'c7ed9d09-7c28-43e6-8677-97921733e09b',
           eventDate: '2023-09-06T09:00:00.000Z',
           title: 'B.B.STREET 26th Anniversary SP Party',
           venue: "川崎club CITTA' & A'TTIC",
           acts: ['Azami'],
         },
         {
-          id: 7,
+          uuid: '64c19a2f-9a5c-4b5a-9050-8b077cd16f4f',
           eventDate: '2023-08-27T09:00:00.000Z',
           title: 'FORWARD RELEASE TOUR 2023',
           venue: '大阪心斎橋VARON',
           acts: ['Azami'],
         },
         {
-          id: 8,
+          uuid: 'f0ebbf10-58fe-4b76-83b7-c20435bc15e2',
           eventDate: '2023-10-08T09:00:00.000Z',
           title: 'sinma pre “ever free”',
           venue: '駒ヶ根G-studio NIRVASH',
